0a047871850b1f9620868a702d50d5def36dc7ce
PRIYANSHU2222 <94793498+PRIYANSHU2222@users.noreply.github.com>
2022-10-09 15:51:31 +0100

    Add files via upload

---
+++ square of a number.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+
+
+    int x, square;
+    printf("enter the number ");
+    scanf("%d",&x);
+    square = x*x;
+    printf("the square of %d is %d,x,square);
+    return 0;
+    
+}
